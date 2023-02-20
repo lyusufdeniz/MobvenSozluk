c068b678c1da2f6ea06e4dcb477176ddef52b9c7
Mert Bulutoglu <mertbulutoglu95@gmail.com>
2023-02-20 17:18:46 +0300

    latest configurations

--- Infrastructure/MobvenSozluk.Persistance/Context/AppDbContext.cs
+++ Infrastructure/MobvenSozluk.Persistance/Context/AppDbContext.cs
@@ -11,6 +11,7 @@ namespace MobvenSozluk.Persistance.Context
 {
     public class AppDbContext : DbContext
     {
+        
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
