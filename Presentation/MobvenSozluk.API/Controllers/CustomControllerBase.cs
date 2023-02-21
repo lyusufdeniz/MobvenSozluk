3477e9ab6eb64392a1aaf9f8fe78da4fd745bafd
Halit Burak Ustundag <hburakustundag@gmail.com>
2023-02-21 12:29:47 +0300

    Create CustomControllerBase.cs

---
+++ Presentation/MobvenSozluk.API/Controllers/CustomControllerBase.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MobvenSozluk.Repository.DTOs.ResponseDTOs;
+
+namespace MobvenSozluk.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomControllerBase : ControllerBase
+    {
+        [NonAction]
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> response)
+        {
+            if (response.StatusCode == 204)
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode
+                };
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
