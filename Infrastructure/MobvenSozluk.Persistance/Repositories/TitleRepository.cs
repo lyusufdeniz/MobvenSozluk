35c6bcf458473066cc97395df39a0759bde972bb
Halit Burak Ustundag <hburakustundag@gmail.com>
2023-03-06 17:31:19 +0300

    Create endpoint for popular titles

--- Infrastructure/MobvenSozluk.Persistance/Repositories/TitleRepository.cs
+++ Infrastructure/MobvenSozluk.Persistance/Repositories/TitleRepository.cs
@@ -21,6 +21,34 @@ namespace MobvenSozluk.Persistance.Repositories
             return await _context.Titles.Include(x => x.Entries).Where(x => x.Id == titleId).SingleOrDefaultAsync();
         }
 
+        public async Task<List<Title>> GetPopularTitlesWithEntries()
+        {
+            var currentDate = DateTime.UtcNow;
+            var startDate = currentDate.AddDays(-7);
+
+            // Get the titles with their related entries, filtering by activity in the last 7 days
+            var popularTitles = await _context.Titles
+                .Include(t => t.Entries)
+                .Where(t => t.IsActive && !t.IsDeleted && t.CreatedDate >= startDate && t.Views >= 0)
+                .OrderByDescending(t => (t.Views * 0.3) + (t.Entries.Count * 0.4) + (t.Entries.Sum(e => e.UpVotes) * 0.3))
+                .Take(20)
+                .ToListAsync();
+
+            // If there is no activity in the last 7 days, get the 20 most popular titles regardless of time
+            if (popularTitles.Count == 0)
+            {
+                popularTitles = await _context.Titles
+                    .Include(t => t.Entries)
+                    .Where(t => t.IsActive && !t.IsDeleted && t.Views >= 0)
+                    .OrderByDescending(t => (t.Views * 0.3) + (t.Entries.Count * 0.4) + (t.Entries.Sum(e => e.UpVotes) * 0.3))
+                    .Take(20)
+                    .ToListAsync();
+            }
+
+            return popularTitles;
+        }
+
+
         public async Task<List<Title>> GetTitlesWithUserAndCategory()
         {
           return await _context.Titles.Include(x => x.User).Include(z => z.Category).ToListAsync();
